7c39aae3030d57e82d044f0742f3170c46a595e3
Anant Kurapati <anant.kurapati@gmail.com>
2022-08-11 18:27:42 +0530

    Update beanstalk.tf
    updated code to run

--- beanstalk.tf
+++ beanstalk.tf
@@ -1,27 +1,27 @@
 
-resource "aws_s3_bucket" "s3-bucket-dao-aurora-test" {
-  bucket = "dao-aurora-test"
+resource "aws_s3_bucket" "s3-bucket-aurora-test" {
+  bucket = "aurora-test"
 }
-resource "aws_s3_object" "s3-object-dao-aurora-test" {
-  bucket = aws_s3_bucket.s3-bucket-dao-aurora-test.id
-  key = "beanstalk/dao-aurora-test"
+resource "aws_s3_object" "s3-object-aurora-test" {
+  bucket = aws_s3_bucket.s3-bucket-aurora-test.id
+  key = "beanstalk/aurora-test"
   source = "target/app.jar"
 }
-resource "aws_elastic_beanstalk_application" "beanstalk-dao-aurora-test" {
-  name = "dao-aurora-test"
-  description = "The description of dao aurora application"
+resource "aws_elastic_beanstalk_application" "beanstalk-aurora-test" {
+  name = "aurora-test"
+  description = "The description of  aurora application"
 }
-resource "aws_elastic_beanstalk_application_version" "beanstalk-dao-aurora-test-version" {
-  application = aws_elastic_beanstalk_application.beanstalk-dao-aurora-test.name
-  bucket = aws_s3_bucket.s3-bucket-dao-aurora-test.id
-  key = aws_s3_object.s3-object-dao-aurora-test.id
-  name = "dao_aurora_test-1.0.4"
+resource "aws_elastic_beanstalk_application_version" "beanstalk-aurora-test-version" {
+  application = aws_elastic_beanstalk_application.beanstalk-aurora-test.name
+  bucket = aws_s3_bucket.s3-bucket-aurora-test.id
+  key = aws_s3_object.s3-object-aurora-test.id
+  name = "aurora_test-1.0.4"
 }
-resource "aws_elastic_beanstalk_environment" "beanstalk-dao-aurora-test-env" {
-  name = "dao-aurora-test-prod"
-  application = aws_elastic_beanstalk_application.beanstalk-dao-aurora-test.name
+resource "aws_elastic_beanstalk_environment" "beanstalk-aurora-test-env" {
+  name = "aurora-test-prod"
+  application = aws_elastic_beanstalk_application.beanstalk-aurora-test.name
   solution_stack_name = "64bit Amazon Linux 2 v3.2.11 running Corretto 8"
-  version_label = aws_elastic_beanstalk_application_version.beanstalk-dao-aurora-test-version.name
+  version_label = aws_elastic_beanstalk_application_version.beanstalk-aurora-test-version.name
   setting {
     name = "SERVER_PORT"
     namespace = "aws:elasticbeanstalk:application:environment"
